011d057f8701d1612b05a901a742d2d1fcb6d45f
Zhi Yong Han <hanzyong0@gmail.com>
2022-02-12 14:12:13 +0800

    post and display messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Mini Message Board', messages });
+});
+
+router.post('/new', function (req, res) {
+  messages.push({ text: req.body.messageText, user: req.body.messageUser, added: new Date() });
+  res.redirect('/');
 });
 
 module.exports = router;
